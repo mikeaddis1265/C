d83105ddcf849d3e173feb39182be13728dfddc8
unknown <michaeladdis78@gmail.com>
2025-04-29 09:24:59 +0300

    intensionally crate bug to test the workflow

--- 0x00-hello_world/tests/test_5_printf.sh
+++ 0x00-hello_world/tests/test_5_printf.sh
@@ -2,7 +2,7 @@
 
 # Run the binary and capture output
 output=$(../5-printf)
-expected="with proper grammar, but the outcome is a piece of art,"
+expected="proper grammar, but the outcome is a piece of art,"
 
 if [ "$output" = "$expected" ]; then
   echo "✅ test_5_printf: PASS"
